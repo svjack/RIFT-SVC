2a39a6a2aac50f4e70afc9c31134bf123c22f813
prismleong <prismleong@163.com>
2024-12-29 16:09:00 +0800

    data preprare

---
+++ prepare.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DATA_DIR="data/finetune"
+
+python scripts/prepare_data_meta.py --data-dir $DATA_DIR
+python scripts/prepare_mel.py --data-dir $DATA_DIR
+python scripts/prepare_rms.py --data-dir $DATA_DIR
+python scripts/prepare_f0.py --data-dir $DATA_DIR
+python scripts/prepare_cvec.py --data-dir $DATA_DIR
+
+python scripts/combine_features.py --data-dir $DATA_DIR
