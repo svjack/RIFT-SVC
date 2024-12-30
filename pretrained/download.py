c68d6e9cb9e61ced20bc2ce0b9e7a13bc71d3dc9
prismleong <prismleong@163.com>
2024-12-30 17:45:40 +0800

    debug

--- pretrained/download.py
+++ pretrained/download.py
@@ -1,5 +1,5 @@
-from huggingface_hub import snapshot_download
+from huggingface_hub import hf_hub_download
 
 
 if __name__ == "__main__":
-    snapshot_download(repo_id="Pur1zumu/RIFT-SVC-modules", local_dir='pretrained')
+    hf_hub_download(repo_id="Pur1zumu/RIFT-SVC-modules", local_dir='pretrained')
