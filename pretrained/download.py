c5c141ae2a22401b9378c17a1e94dc260cdb0b2a
prismleong <prismleong@163.com>
2024-12-30 17:50:06 +0800

    debug

--- pretrained/download.py
+++ pretrained/download.py
@@ -1,5 +1,12 @@
-from huggingface_hub import hf_hub_download
+from huggingface_hub import snapshot_download
 
 
 if __name__ == "__main__":
-    hf_hub_download(repo_id="Pur1zumu/RIFT-SVC-modules", local_dir='pretrained')
+    model_path = snapshot_download(
+        repo_id="Pur1zumu/RIFT-SVC-modules",
+        local_dir='pretrained',
+        local_dir_use_symlinks=False,  # Don't use symlinks
+        local_files_only=False,        # Allow downloading new files
+        ignore_patterns=["*.git*"],    # Ignore git-related files
+        resume_download=True           # Resume interrupted downloads
+    )
