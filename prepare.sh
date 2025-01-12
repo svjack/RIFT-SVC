a6ea02ec5b3c4b112fad9c50346cdbf8b3a957b9
prismleong <prismleong@163.com>
2025-01-12 16:17:36 +0800

    update

--- prepare.sh
+++ prepare.sh
@@ -7,5 +7,6 @@ python scripts/prepare_mel.py --data-dir $DATA_DIR
 python scripts/prepare_rms.py --data-dir $DATA_DIR
 python scripts/prepare_f0.py --data-dir $DATA_DIR
 python scripts/prepare_cvec.py --data-dir $DATA_DIR
+python scripts/prepare_whisper.py --data-dir $DATA_DIR
 
 python scripts/combine_features.py --data-dir $DATA_DIR
